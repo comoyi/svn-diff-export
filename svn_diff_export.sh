995bd9dbdd8de1ef385a2c8f5579f16ceb4d3e02
Michael Chi <chicong@outlook.com>
2015-08-12 09:16:29 +0800

    Update svn_diff_export.sh

--- svn_diff_export.sh
+++ svn_diff_export.sh
@@ -12,35 +12,35 @@ color_yellow='\033[1;33m'
 revision_from=$1 # start version
 revision_to=$2 # end version
 repository_url=$3 # repository url
-target_path=$4 # path to store the export files
+destination_path=$4 # path to store the export files
+
+# show summarize
+show_summarize()
+{
+    printf "${color_yellow}> Summarize${color_reset}\n"
+    svn diff -r ${revision_from}:${revision_to} --summarize ${repository_url} | sort
+}
 
 # export changed files
 export_change()
 {
-    printf "${color_yellow}> Start exporting changed files between revision ${color_red}${revision_from}${color_yellow} and revision ${color_red}${revision_to}${color_yellow} to ${color_blue}${target_path}${color_reset}\n";
-    changed_files=`svn diff -r${revision_from}:${revision_to} --summarize ${repository_url} | sort | awk '/^[AM]/{print $NF}'`
+    printf "${color_yellow}> Start exporting changed files between revision ${color_red}${revision_from}${color_yellow} and revision ${color_red}${revision_to}${color_yellow} to ${color_blue}${destination_path}${color_reset}\n";
+    changed_files=`svn diff -r ${revision_from}:${revision_to} --summarize ${repository_url} | sort | awk '/^[AM]/{print $NF}'`
     for file in ${changed_files}
     do
         tmp_svn_full_file_name=${file}
         tmp_full_file_name=${file#*${repository_url}}
         tmp_file_path=`dirname ${tmp_full_file_name}`
         tmp_file_name=${file##*/}
-        tmp_target_full_file_name=${target_path}${tmp_full_file_name}
-        tmp_target_path=${target_path}${tmp_file_path}
-        #printf "svn_full_name: ${tmp_svn_full_file_name} full_file_name: ${tmp_full_file_name} file_path: ${tmp_file_path} file_name: ${tmp_file_name} target_path: ${tmp_target_path}\n"
-        if [ ! -d ${tmp_target_path} ]; then
-            mkdir -p ${tmp_target_path} 
+        tmp_target_full_file_name=${destination_path}${tmp_full_file_name}
+        tmp_destination_path=${destination_path}${tmp_file_path}
+        #printf "svn_full_name: ${tmp_svn_full_file_name} full_file_name: ${tmp_full_file_name} file_path: ${tmp_file_path} file_name: ${tmp_file_name} destination_path: ${tmp_destination_path}\n"
+        if [ ! -d ${tmp_destination_path} ]; then
+            mkdir -p ${tmp_destination_path} 
         fi
         printf "${tmp_target_full_file_name}\n"
-        svn export --depth empty --force -q -r${revision_to} ${tmp_svn_full_file_name} ${tmp_target_path} 
+        svn export --depth 'empty' --force -q -r ${revision_to} ${tmp_svn_full_file_name} ${tmp_destination_path} 
     done
-}
-
-# show summarize
-show_summarize()
-{
-    printf "${color_yellow}> Summarize${color_reset}\n"
-    svn diff -r${revision_from}:${revision_to} --summarize ${repository_url} | sort
 }
 
 printf "${color_green}------- start -------${color_reset}\n";
